8381c5cdcebec0ca0fcdc477f244996ff5fa32a7
Karl Shouler <kshouler@gmail.com>
2019-02-02 18:41:39 -0500

    Add typescript.

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Head from './head'
+
+export default class App extends React.Component <{}> {
+        
+        render() {
+            return (
+                <div>
+                    <Head/>
+                    <GridLeft>
+                        <Title>This is Karl\'s website.</Title>
+                    </GridLeft>
+                </div>
+            );
+        }
+}
+
+// Tachyons + styled-components -- https://medium.com/@jikkujose/tachyons-styled-components-pure-joy-6173d3888548
+const Title = styled.div.attrs({
+    className: 'f2 green',
+})``;
+
+const GridLeft = styled.div.attrs({
+    className: 'fl w-50',
+})`
+    padding: 32px;
+    padding-top: 200px;
+`;
