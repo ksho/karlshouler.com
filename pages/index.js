ff4125444f3c2706f44e9f95e8376d0c37f61f7e
Karl Shouler <kshouler@gmail.com>
2019-02-02 15:05:35 -0500

    Setup site scaffold. nextjs, .gitignore, styled-components, and tachyons.

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default () => <Test>Welcdddddome to karlshouler.com</Test>
+
+
+const Test = styled.div`
+    color: green;
+`;
+
+const Body = styled.body``;
