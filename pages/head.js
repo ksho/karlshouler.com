d58b0ebb889ec4306832dc7aa8b7a963a937cbf7
Karl Shouler <kshouler@gmail.com>
2019-06-21 23:22:13 -0400

    Update SEO and favicon.

--- pages/head.js
+++ pages/head.js
@@ -1,16 +1,23 @@
 import Head from 'next/head'
 
-export default ({ title='Karl Shouler', description='' }) =>
+export default ({
+    title='Karl Shouler',
+    description='Karl Shouler is a Software Developer in Philadelphia.'
+}) =>
     <Head>
         <meta charSet="utf-8" />
-        <title>{title} ✷ karlshouler.com</title>
+        <title>{ title }</title>
         <meta name="author" content="Karl Shouler" />
-        <meta name="description" content={description} />
+        <meta name="description" content={ description } />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:title" content={title} />
+        <meta property="og:title" content={ title } />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="" />
-        <meta property="og:description" content={description} />
+        <meta property="og:description" content={ description } />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="/static/tachyons.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
     </Head>
