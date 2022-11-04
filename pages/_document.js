aeb6a7e73855f5da675ce57b77b5b4669965ec1b
Karl Shouler <kshouler@gmail.com>
2022-11-04 16:13:33 -0400

    Fix html errors.

--- pages/_document.js
+++ pages/_document.js
@@ -1,27 +1,31 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import styled from 'styled-components';
 
 export default class MyDocument extends Document {
-    static getInitialProps ({ renderPage }) {
+    static async getInitialProps (ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+
         // styled-components in nextjs -- https://jsramblings.com/2017/11/27/using-styled-components-with-next-js.html
         const sheet = new ServerStyleSheet()
-        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
+        const page = ctx.renderPage(App => props => sheet.collectStyles(<App {...props} />))
         const styleTags = sheet.getStyleElement()
-        return { ...page, styleTags }
+        
+        return { ...initialProps, ...page, styleTags }
     }
 
     render () {
         return (
-            <html>
+            <Html>
                 <Head>
-                {this.props.styleTags}
+                    {this.props.styleTags}
+                    <link rel="stylesheet" href="/static/tachyons.min.css" />
                 </Head>
                 <body style={bodyStylePop}>
                     <Main />
                     <NextScript />
                 </body>
-            </html>
+            </Html>
         )
     }
 }
